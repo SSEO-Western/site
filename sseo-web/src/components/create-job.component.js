0cf1217798913c44cb88878720a91b3fba15dad0
gsimon321 <60612435+gsimon321@users.noreply.github.com>
2020-06-10 21:16:37 -0400

    small changes from todo to job

--- sseo-web/src/components/create-job.component.js
+++ sseo-web/src/components/create-job.component.js
@@ -5,35 +5,35 @@ export default class JobsList extends Component {
 constructor(props) {
     super(props);
 
-    this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
-    this.onChangeTodoClub = this.onChangeTodoClub.bind(this);
-    this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
+    this.onChangeJobDescription = this.onChangeJobDescription.bind(this);
+    this.onChangeJobClub = this.onChangeJobClub.bind(this);
+    this.onChangeJobPriority = this.onChangeJobPriority.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
 
 
     this.state = {
-        todo_description: '',
-        todo_club: '',
-        todo_priority: '',
-        todo_completed: false
+        job_description: '',
+        job_club: '',
+        job_priority: '',
+        job_completed: false
     }
 
 }
-    onChangeTodoDescription(e) {
+    onChangeJobDescription(e) {
         this.setState({
-            todo_description: e.target.value
+            job_description: e.target.value
         });
     }
 
-    onChangeTodoClub(e) {
+    onChangeJobClub(e) {
         this.setState({
-            todo_club: e.target.value
+            job_club: e.target.value
         });
     }
-    onChangeTodoPriority(e) {
+    onChangeJobPriority(e) {
         this.setState({
-            todo_priority: e.target.value
+            job_priority: e.target.value
         });
     }
 
@@ -42,40 +42,40 @@ constructor(props) {
         
         //put the onsumbit logic here instead of the console
         console.log('Form Submitted');
-        console.log(`Todo Description: ${this.state.todo_description}`)
-        console.log(`Todo Club: ${this.state.todo_club}`)
-        console.log(`Todo Priority: ${this.state.todo_priority}`)
-        console.log(`Todo Completed: ${this.state.todo_completed}`)
+        console.log(`Job Description: ${this.state.job_description}`)
+        console.log(`Job Club: ${this.state.job_club}`)
+        console.log(`Job Priority: ${this.state.job_priority}`)
+        console.log(`Job Completed: ${this.state.job_completed}`)
 
 
 
         this.setState({
-            todo_description: '',
-            todo_club: '',
-            todo_priority: '',
-            todo_completed: false
+            job_description: '',
+            job_club: '',
+            job_priority: '',
+            job_completed: false
         })
     }
 
     render() {
         return (
             <div style={{marginTop: 20}}>
-                <h3>Create New Todo</h3>
+                <h3>Create New Job</h3>
                 <form onSubmit={this.onSubmit}>
                 <div className = "form-group">
                     <label>Description: </label>
                     <input type = "text"
                         className = "form-control"
-                        value = {this.state.todo_description}
-                        onChange={this.onChangeTodoDescription}/>
+                        value = {this.state.job_description}
+                        onChange={this.onChangeJobDescription}/>
 
                 </div>
                 <div className = "form-group">
                     <label>Club: </label>
                     <input type = "text"
                         className = "form-control"
-                        value = {this.state.todo_club}
-                        onChange={this.onChangeTodoClub}/>
+                        value = {this.state.job_club}
+                        onChange={this.onChangeJobClub}/>
 
                 </div>
                 <div className = "form-group">
@@ -85,8 +85,8 @@ constructor(props) {
                                 name = "priorityOptions"
                                 id = "priorityLow"
                                 value = "Low"
-                                checked = {this.state.todo_priority === 'Low'}
-                                onChange = {this.onChangeTodoPriority}
+                                checked = {this.state.job_priority === 'Low'}
+                                onChange = {this.onChangeJobPriority}
                         />
                         <label className = "form-check-label">Low</label>
                     </div>
@@ -97,8 +97,8 @@ constructor(props) {
                                 name = "priorityOptions"
                                 id = "priorityMedium"
                                 value = "Medium"
-                                checked = {this.state.todo_priority === 'Medium'}
-                                onChange = {this.onChangeTodoPriority}
+                                checked = {this.state.job_priority === 'Medium'}
+                                onChange = {this.onChangeJobPriority}
                         />
                         <label className = "form-check-label">Medium</label>
                     </div>
@@ -108,8 +108,8 @@ constructor(props) {
                                 name = "priorityOptions"
                                 id = "priorityHigh"
                                 value = "High"
-                                checked = {this.state.todo_priority === 'High'}
-                                onChange = {this.onChangeTodoPriority}
+                                checked = {this.state.job_priority === 'High'}
+                                onChange = {this.onChangeJobPriority}
                         />
                         <label className = "form-check-label">High</label>
                     </div>
